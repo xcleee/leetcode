58faf4797e19e6ac91ba33e81dfb537a27cfdded
xclee <xiaochaoi2010@gmail.com>
2013-09-03 20:13:49 +0800

    update permutation algor

--- permutations.cpp
+++ permutations.cpp
@@ -7,24 +7,65 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
-void perm(vector<int> &num, vector<vector<int> >& result, int start, int end)
+// k is the number currently chosen to swap with the element after it
+void perm(vector<int> &num, vector<vector<int> >& result, int k, int end)
 {
-    if (start < end - 1)
+    if (k < end - 1)
     {
-        perm(num, result, start+1, end);// num[0],(num[1],...,num[end-1])
-        for (int i = start+1; i < end; i++)//num[i],(num[1], ..., num[i], ..., num[end-1])
+        for (int i = k; i < end; i++)
         {
-            swap(num[start], num[i]);
-            perm(num, result, start+1, end);
-            swap(num[start], num[i]);
+            //num[i],(num[1], ..., num[k], ..., num[end-1])
+            swap(num[k], num[i]);
+            perm(num, result, k+1, end);
+            swap(num[k], num[i]);
         }
     }
     else
         result.push_back(num);
+}
+
+bool canSwap(vector<int>& num, int k, int m)
+{
+    for (int i = k; i < m; i++)
+        if (num[i] == num[m])
+            return false;
+    return true;
+}
+
+void permWithRepeatElement(vector<int>& num, vector<vector<int> >& result, int k, int end)
+{
+    if (k < end - 1)
+    {
+        for (int i = k; i < end; i++)
+        {
+            if (canSwap(num, k, i))
+            {
+                //num[i],(num[1], ..., num[k], ..., num[end-1])
+                swap(num[k], num[i]);
+                permWithRepeatElement(num, result, k+1, end);
+                swap(num[k], num[i]);
+            }
+        }
+    }
+    else
+        result.push_back(num);
+}
+
+vector<vector<int> > permUnique(vector<int>& num)
+{
+    vector<bool> used(num.size(), false);
+    vector<vector<int> > result;
+
+    sort(num.begin(), num.end());
+
+    permWithRepeatElement(num, result, 0, num.size());
+
+    return result;
 }
 
 void printResult(vector<vector<int> >& result)
@@ -64,6 +105,18 @@ int main()
     cout << "test case 3" << endl;
     num.push_back(1);
     perm(num, result, 0, num.size());
+    printResult(result);
+
+    vector<vector<int> >().swap(result);
+    vector<int>().swap(num);
+
+    cout << "test case 4" << endl;
+    int test4[] = {1,2,2};
+    for (int i = 0; i < sizeof(test4)/sizeof(test4[0]); i++)
+        num.push_back(test4[i]);
+
+    result = permUnique(num);
+
     printResult(result);
 
     return 0;
